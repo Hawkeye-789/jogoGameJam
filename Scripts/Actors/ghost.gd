53864dea2d5f357feddc6b3f2687c2c168a13aa9
Vinícius Souza <viniso1903@gmail.com>
2025-07-15 18:23:04 -0300

    Base do Fantasma feito

--- Scripts/Actors/ghost.gd
+++ Scripts/Actors/ghost.gd
@@ -1,23 +1,27 @@
 extends CharacterBody2D
 
 @export var movement_speed : float
-@export var running_speed_boost : float
+@onready var line_of_sight := $"Line of Sight" as RayCast2D
+@onready var sight_area := $"Sight Area/Collision" as CollisionShape2D
 
-var character_direction : Vector2
-var running : bool = false
+var player_target : CharacterBody2D
+var direction : Vector2
+var player_position : Vector2
+var player_is_nearby: bool = false
 
 func _physics_process(delta: float) -> void:
-	character_direction.x = Input.get_axis("left", "right")
-	character_direction.y = Input.get_axis("up", "down")
-	character_direction = character_direction.normalized()
-	running = Input.is_action_pressed("run")
+	direction = player_position.normalized()
+	velocity = direction * movement_speed
 	
-	if character_direction:
-		velocity = character_direction * movement_speed
-		if running:
-			velocity *= running_speed_boost
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, movement_speed)
-		
-	move_and_slide()
-	
+	if player_is_nearby:
+		player_position = player_target.global_position - global_position
+		line_of_sight.target_position = player_position
+		if line_of_sight.get_collider() == player_target:
+			move_and_slide()
+
+func _player_entered_sight_area(player: Node2D) -> void:
+	player_target = player
+	player_is_nearby = true
+
+func _player_exited_sight_area(player: Node2D) -> void:
+	player_is_nearby = false
